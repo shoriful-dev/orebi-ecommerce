58998c3cce39129bbfeb2d8ed8c57c3f5315551e
shorifil-dev <cmshohelrana50@gmail.com>
2024-12-08 15:24:48 +0600

    change

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@
 
 import Container from "../components/Container";
 import Flex from "../components/Flex";
-import Pagination from "../components/Pagination";
+
 
 const Shop = () => {
   return (
@@ -11,7 +11,7 @@ const Shop = () => {
         <Flex className={'pt-[200px]'}>
           <div className="w-3/12">Shop by Category</div>
           <div className="w-9/12">
-          <Pagination itemsPerPage={12} />
+          
           </div>
         </Flex>
       </Container>
