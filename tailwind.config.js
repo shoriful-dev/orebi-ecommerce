7f869250de9f735d5582193e95577f49ca766cbe
shorifil-dev <cmshohelrana50@gmail.com>
2025-03-05 13:38:43 +0600

    setup Done Motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,55 @@
+const {
+  default: flattenColorPalette,
+} = require('tailwindcss/lib/util/flattenColorPalette');
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      sm: "577px",
+      sm: '577px',
       // => @media (min-width: 640px) { ... }
 
-      md: "768px",
+      md: '768px',
       // => @media (min-width: 768px) { ... }
 
-      lg: "1024px",
+      lg: '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      xl: "1280px",
+      xl: '1280px',
       // => @media (min-width: 1280px) { ... }
 
-      "2xl": "1536px",
+      '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       screens: {
-        xs: { min: "320px", max: "576px" },
+        xs: { min: '320px', max: '576px' },
       },
       maxWidth: {
-        headerWidth: "1500px",
+        headerWidth: '1500px',
       },
       fontFamily: {
-        DM_Sans: ["DM Sans"],
+        DM_Sans: ['DM Sans'],
       },
       colors: {
-        categoryBg: "#F5F5F3",
-        placeHolderTC: "#C4C4C4",
-      }
+        categoryBg: '#F5F5F3',
+        placeHolderTC: '#C4C4C4',
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
